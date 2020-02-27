91399695fa21f6cede5d357352cf1c5e9a48a28b
lianglanlan <799679063@qq.com>
2020-02-27 23:58:54 +0800

    寻找问题所在

--- app.js
+++ app.js
@@ -19,3 +19,8 @@ http
     })
 
 //这时候在node中执行app.js，在浏览器中打开 http://localhost:3010/ 会发现浏览器一直处于请求中
+/*
+原因是浏览器在收到响应内容HTML时，从上到下开始解析， 当在解析过程中遇到：
+link/script/img/iframe/video/audio等具有src或者href属性（a标签的href属性除外，只有点击时才会执行请求）时
+浏览器会自动对这些资源再次发送请求
+*/
