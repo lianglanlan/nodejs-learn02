04ff0482edfb4a3775bde38c604cdca372d421ff
lianglanlan <799679063@qq.com>
2020-02-28 14:39:01 +0800

    添加404页面

--- app.js
+++ app.js
@@ -18,13 +18,19 @@ http
                 }
                 res.end(data)
             })
+        } else {    //未访问到添加404
+            fs.readFile('./views/404.html', (err, data) => {
+                if (err) {
+                    return res.end('404 Not Found')
+                }
+                res.end(data)
+            })
+
         }
     })
     .listen(3010, () => {
         console.log('running')
     })
-
-//这时候在node中执行app.js，在浏览器中打开 http://localhost:3010/ 会发现控制台中打印出'静态资源/public/node_modules/bootstrap/dist/css/bootstrap.css未找到'
 
 /*
 原因是浏览器在收到响应内容HTML时，从上到下开始解析， 当在解析过程中遇到：
@@ -32,5 +38,4 @@ link/script/img/iframe/video/audio等具有src或者href属性（a标签的href�
 浏览器会自动对这些资源再次发送请求
 
 为了方便的统一这些静态资源，可与约定将所有的静态资源放于public目录中,并对这些请求进行处理。这样可以很灵活的控制哪些代码用户可以访问到（页面、public中的静态资源），哪些不能被用户访问到（例如app.js）
-
 */
